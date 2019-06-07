b7910c02f87d36404c8c719e8f950363a55b590e
rfripp2 <juangonzalez465@gmail.com>
2019-06-07 16:14:34 -0300

    adding fakeFilter

--- app.js
+++ app.js
@@ -27,6 +27,15 @@ function fakeForEach(arr, callback) {
   }
 }
 
+// fakeFilter function
 
-
-
+function fakeFilter(arr, callback) {
+  let filtered = [];
+  for (let i = 0; i < arr.length; i++) {
+    let result = callback(arr[i]);
+    if (result) {
+      filtered.push(arr[i]);
+    }
+  }
+  return filtered;
+}
