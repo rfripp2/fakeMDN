0b539343c3633d03a81cac9a58b8cd63b91c9631
rfripp2 <juangonzalez465@gmail.com>
2019-06-07 10:38:08 -0300

    Update app.js
    adding fakeIncludes function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //fakeIncludes function
-fakeIncludes function fakeIncludes(arr, element) {
+function fakeIncludes(arr, element) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] == element) {
       return true;
